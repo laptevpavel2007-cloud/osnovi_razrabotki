26b94a0b915015eba1318f7491c5b7e86f44d522
Neko_LPN <laptevpavel2007@gmail.com>
2026-03-24 16:50:17 +0800

    update

--- testing/testing_tdd/setup.py
+++ testing/testing_tdd/setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "ndfl",
+    name = "ndfl-lpn",
     version = "0.0.0",
-
-    package_dir = {"": "src"},
-    packages = find_packages(where="src")
+    long_description="Tax calculator",
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    author="Laptev Pavel",
+    author_email="Laptevpavel2007@gmail.com"
 )
